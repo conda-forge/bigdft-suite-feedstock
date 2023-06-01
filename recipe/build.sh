1fe0e7741d7a481589eaa1feb7ae2020f5747ddf
William Dawson <william.dawson@riken.jp>
2023-06-01 12:05:52 +0900

    Try in futiles config directory

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,14 @@
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* futile/
-
 # Files
 cp $RECIPE_DIR/conda.rc .
-
-# Build
-export JHBUILD_RUN_AS_ROOT="please do it"
 mkdir build
 cd build
+
+# Autotools stuff
+export JHBUILD_RUN_AS_ROOT="please do it"
 python ../Installer.py -y autogen
+cp $BUILD_PREFIX/share/gnuconfig/config.* ../futile/config/
+
+# Run Installer
 python ../Installer.py -y build -f ../conda.rc
 
 # Environment variables
