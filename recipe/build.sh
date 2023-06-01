70f72ca5c1d476359324e3a9529570d288d7bc23
William Dawson <william.dawson@riken.jp>
2023-06-01 17:07:51 +0900

    Maybe this will work...

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 # Files
 cp $RECIPE_DIR/conda.rc .
+tar xvf tarballs/GaIn-1.0.tar.gz
+
 mkdir build
 cd build
 
@@ -7,6 +9,13 @@ cd build
 export JHBUILD_RUN_AS_ROOT="please do it"
 python ../Installer.py -y autogen
 cp $BUILD_PREFIX/share/gnuconfig/config.* ../futile/config/
+cp $BUILD_PREFIX/share/gnuconfig/config.* ../GaIn-1.0/
+
+# Make sure the Fortran compiler is correct for openmpi
+if [ "$mpi" == "openmpi" ]
+then
+    export OMPI_FC=$FC
+fi
 
 # Run Installer
 python ../Installer.py -y build -f ../conda.rc
