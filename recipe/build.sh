9f81aa8bf84fe98464f3711b6377c257adb353c6
William Dawson <william.dawson@riken.jp>
2023-06-01 13:09:19 +0900

    copy before autogen?

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,8 @@ cd build
 
 # Autotools stuff
 export JHBUILD_RUN_AS_ROOT="please do it"
-python ../Installer.py -y autogen
 cp $BUILD_PREFIX/share/gnuconfig/config.* ../futile/config/
+python ../Installer.py -y autogen
 
 # Run Installer
 python ../Installer.py -y build -f ../conda.rc
